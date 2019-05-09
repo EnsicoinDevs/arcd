2a6e243c811d2d2b37c23992fac18a6ba87a0e8b
Quentin Boyer <qbsecond@gmail.com>
2019-05-09 11:52:19 +0200

    grpc update

--- build.rs
+++ build.rs
@@ -1,15 +1,15 @@
 extern crate tower_grpc_build;
 
 fn main() {
-    tower_grpc_build::Config::new()
-        .enable_server(false)
-        .enable_client(true)
-        .build(&["proto/miner.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
+    /*tower_grpc_build::Config::new()
+    .enable_server(false)
+    .enable_client(true)
+    .build(&["proto/miner.proto"], &["proto"])
+    .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));*/
 
     tower_grpc_build::Config::new()
         .enable_server(true)
         .enable_client(false)
-        .build(&["proto/node.proto"], &["proto"])
+        .build(&["protos/node.proto"], &["protos"])
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
