efceab68694fc888844ccc850b8e2b7612c3ee92
Quentin Boyer <qbsecond@gmail.com>
2019-05-21 00:14:17 +0200

    Please be good

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -4,5 +4,5 @@ pub const DEFAULT_PORT: &str = "4224";
 pub const IP: &str = "127.0.0.1";
 pub const DEFAULT_MAX_CONN: &str = "42";
 pub const IMPLEMENTATION: &str = "another-rust-coin";
-pub const DEFAULT_GRPC_PORT: &str = "4242";
+pub const DEFAULT_GRPC_PORT: &str = "4225";
 pub const TIME_BEETWEEN_BLOCKS: u64 = 302400;
